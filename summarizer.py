9ee0cca10c94a609484b1a3f406e4bb31d75657d
rahulroxx <rahulyupz@gmail.com>
2016-08-12 16:40:34 +0530

    update code

--- summarizer.py
+++ summarizer.py
@@ -52,14 +52,13 @@ def textrank(document):
 
 
 def summarize():
-    path = "C:\\Users\\rahul\\Downloads\\Video\\video.mp4"
-    para = "I want to suggest a good way to be fit and this might not be the only one but Yeah It's possible to do so. It kind of amazing whey people like you are really stupid about the subject. that is not a problem but it is a matter of concern > People grow UP!!!"
+    # para = "I want to suggest a good way to be fit and this might not be the only one but Yeah It's possible to do so. It kind of amazing whey people like you are really stupid about the subject. that is not a problem but it is a matter of concern > People grow UP!!!"
+    para = "BB u r I have watched all ur videos...literally all... all are awesome... wishing that you will be most viewed artist on YouTube n most liked in FB... my DP status n everything features U... ur timing is just amazing...kha se late ho itne sare ideas"
     # payload = {'apikey': 'a429a338-07a1-4b6e-bd46-c75b1fab8c89', 'text': para}
     payload = {'apikey': 'a429a338-07a1-4b6e-bd46-c75b1fab8c89', 'url': 'https://www.youtube.com/watch?v=OkP8BAwfO24'}
 
     # r = requests.get('http://api.idolondemand.com/1/api/sync/extractconcepts/v1', params=payload)
-    # r = requests.get('https://api.havenondemand.com/1/api/sync/analyzesentiment/v1', params=payload)
-    r = requests.get('https://api.havenondemand.com/1/api/async/recognizespeech/v1', params=payload)
+    r = requests.get('https://api.havenondemand.com/1/api/sync/analyzesentiment/v1', params=payload)
     json_r = json.loads(r.text)
     print json_r
 
