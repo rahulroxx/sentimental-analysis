563796ea624eedee5881c403bed1db7f9eb0711f
rahulroxx <rahulyupz@gmail.com>
2016-08-12 12:53:39 +0530

    added gitignore

--- sentiments.py
+++ sentiments.py
@@ -19,6 +19,7 @@ def sentiment(text):
     """
     words = pattern_split.split(text.lower())
     sentiments = map(lambda word: afinn.get(word, 0), words)
+    print sentiments
     if sentiments:
         # How should you weight the individual word sentiments? 
         # You could do N, sqrt(N) or 1 for example. Here I use sqrt(N)
@@ -32,14 +33,14 @@ def sentiment(text):
 
 if __name__ == '__main__':
     # Single sentence example:
-    text = "Finn is stupid and idiotic"
-    print("%6.2f %s" % (sentiment(text), text))
+    # text = "Finn is stupid and idiotic"
+    # print("%6.2f %s" % (sentiment(text), text))
     
     # No negation and booster words handled in this approach
-    text = "Finn is only a tiny bit stupid and not idiotic. What I think about is good actually is really sarcastic"
-    print("%6.2f %s" % (sentiment(text), text))
+    # text = "Finn is only a tiny bit stupid and not idiotic. What I think about is good actually is really sarcastic"
+    # print("%6.2f %s" % (sentiment(text), text))
 
     # Test 
-    text = "Finn is only a tiny bit stupid and not idiotic. What I think about is good actually is really sarcastic"
+    # text = "Finn is only a tiny bit stupid and not idiotic. What I think about is good actually is really sarcastic"
     text = "I want to suggest a good way to be fit and this might not be the only one but Yeah It's possible to do so. It kind of amazing whey people like you are really stupid about the subject. that is not a problem but it is a matter of concern > People grow UP!!!"
     print("%6.2f %s" % (sentiment(text), text))
